211087fc5a91b1e370f51fe686ff5fddd41389ce
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2021-07-21 20:35:48 +0200

    Initial Commit

---
+++ PlayCardNumberingGameSwiftUI/PlayCardNumberingGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayCardNumberingGameSwiftUIApp.swift
+//  PlayCardNumberingGameSwiftUI
+//
+//  Created by Tomislav Gelesic on 21.07.2021..
+//
+
+import SwiftUI
+
+@main
+struct PlayCardNumberingGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
